947d190a7c4240c626482149152c3ac931405390
Ales Zatloukal <ajazat@seznam.cz>
2013-06-26 10:27:50 +0200

    Updated for version 2.1

--- src/me/DRIVEmaxx/DRIVEmaxxLocator/DRIVEmaxxLocator.java
+++ src/me/DRIVEmaxx/DRIVEmaxxLocator/DRIVEmaxxLocator.java
@@ -53,7 +53,7 @@ public class DRIVEmaxxLocator extends JavaPlugin
     }
     if (cmd.getName().equalsIgnoreCase("dlver"))
     {
-      sender.sendMessage(ChatColor.GOLD + "[DRIVEmaxxLocator] " + ChatColor.GREEN + "Verze pluginu je 2.0 !");
+      sender.sendMessage(ChatColor.GOLD + "[DRIVEmaxxLocator] " + ChatColor.GREEN + "Verze pluginu je 2.1 !");
     }
     return true;
   }
